0bbac23f289ffc5d375eade58378587b99f6f990
Aybars Cengaver <aybarscengaver@gmail.com>
2014-08-07 18:11:57 +0300

    meh.

--- safran.php
+++ safran.php
@@ -1,8 +1,7 @@
 #!/usr/bin/php
 <?php
 $feed  = 'http://www.safran.io/feed.rss';
-$data = simplexml_load_file($feed);
-$items = array_slice($data->channel->item,0,10);
+$data = simplexml_load_file($feed); 
 
 foreach($data->channel->item as $item){
 	echo "\n\xF0\x9F\x94\xB9  \033[0;37m{$item->title}\033[0m ";
