95e092c3896b538af5af0a92161e1f5d5de89834
Aybars Cengaver <aybarscengaver@gmail.com>
2014-08-07 18:02:48 +0300

    Direk kopya daha iyi

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 path=${PWD}
-sudo ln -s $path/safran.php /usr/bin/safran
+#sudo ln -s $path/safran.php /usr/bin/safran
+sudo cp ./safran.php /usr/bin/safran
 sudo chmod +x /usr/bin/safran
 echo "Installation complete"
